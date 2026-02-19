7b7b05538470e63140fca43b8e37f68cf231b096
Jenn Wang <jenn.wang@slalom.com>
2026-02-19 11:59:19 -0500

    Fix typo bug related to env

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "environment" {
   description = "Environment name (dev, test, prod)"
   type        = string
   validation {
-    condition     = contains(["dev", "test", "prod"], var.environment)
+    condition     = contains(["DEV", "TEST", "PROD"], var.environment)
     error_message = "Environment must be dev, test, or prod."
   }
 }
