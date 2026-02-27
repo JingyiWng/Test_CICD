1c3e12622db6483984c596438b3786c6dd4e0e30
Jenn Wang <jenn.wang@slalom.com>
2026-02-27 16:37:46 -0500

    Use ADLS as Terraform backend for state management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.0"
   
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "saterraformstatetodoapp"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
